7ffac4a1941db0b94407175080c8c95af8e69029
Joseph Hamilton <jlhamilton0408@gmail.com>
2024-12-20 16:09:44 -0600

    Moved testing files from src/ to src/testing/

--- src/main.rs
+++ src/main.rs
@@ -4,18 +4,18 @@ pub mod ast;
 pub mod parser;
 
 fn main() {
-    let path = "src/test_code.scn".to_string();
+    let path = "src/testing/test_code.scn".to_string();
     let code = std::fs::read_to_string(&path).unwrap();
     
     // lexer
     let tokens = lexer::lex(&code, &path);
     
     let json = serde_json::to_string_pretty(&tokens).unwrap();
-    std::fs::write("src/lexer.json", json).unwrap();
+    std::fs::write("src/testing/lexer.json", json).unwrap();
 
     // parser    
     let ast = parser::parse(tokens, &path, &code);
 
     let fmt_json = serde_json::to_string_pretty(&ast).unwrap();
-    std::fs::write("src/parser.json", fmt_json).unwrap();
+    std::fs::write("src/testing/parser.json", fmt_json).unwrap();
 }
