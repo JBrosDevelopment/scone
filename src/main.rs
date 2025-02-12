5edf2534a6472670f5c63b88752c2b93dceb7193
Joseph Hamilton <jlhamilton0408@gmail.com>
2025-02-11 20:16:04 -0600

    Update output file names and clean up test data

--- src/main.rs
+++ src/main.rs
@@ -11,11 +11,11 @@ fn main() {
     let tokens = lexer::lex(&code, &path);
     
     let json = serde_json::to_string_pretty(&tokens).unwrap();
-    std::fs::write("src/testing/lexer.json", json).unwrap();
+    std::fs::write("src/testing/lexer.out.json", json).unwrap();
 
     // parser    
     let ast = parser::parse(tokens, &path, &code);
 
     let fmt_json = serde_json::to_string_pretty(&ast).unwrap();
-    std::fs::write("src/testing/parser.json", fmt_json).unwrap();
+    std::fs::write("src/testing/parser.out.json", fmt_json).unwrap();
 }
