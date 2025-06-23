d4ddd8facd705b9e6d1a97bf31e8fc686406500a
Joseph Hamilton <jlhamilton0408@gmail.com>
2025-06-23 17:42:53 -0500

    Refactor vector type definitions and update related functions for consistency; add new function_call and generics implementations.

--- src/testing/test_code_translate.c
+++ src/testing/test_code_translate.c
@@ -8,9 +8,9 @@ typedef struct {                                                    \
     type* data;                                                     \
     size_t size;                                                    \
     size_t capacity;                                                \
-} Vec_##type;                                                       \
-Vec_##type Vec_##type##_new(type* data, size_t length) {            \
-    Vec_##type v;                                                   \
+} Vec_lt_##type##_gt;                                                       \
+Vec_lt_##type##_gt Vec_lt_##type##_gt_new(type* data, size_t length) {            \
+    Vec_lt_##type##_gt v;                                                   \
     v.size = length;                                                \
     v.capacity = (length > 0) ? length : 1;                         \
     v.data = malloc(sizeof(type) * v.capacity);                     \
@@ -21,25 +21,25 @@ Vec_##type Vec_##type##_new(type* data, size_t length) {            \
     memcpy(v.data, data, sizeof(type) * length);                    \
     return v;                                                       \
 }                                                                   \
-type* Vec_##type##_get(Vec_##type* v, size_t index) {               \
+type* Vec_lt_##type##_gt_get(Vec_lt_##type##_gt* v, size_t index) {               \
     if (index >= v->size) {                                         \
         fprintf(stderr, "Out of bounds\n");                         \
         exit(EXIT_FAILURE);                                         \
     }                                                               \
     return &v->data[index];                                         \
 }                                                                   \
-void Vec_##type##_push(Vec_##type* v, type element) {               \
+void Vec_lt_##type##_gt_push(Vec_lt_##type##_gt* v, type element) {               \
     if (v->size == v->capacity) {                                   \
         v->capacity *= 2;                                           \
         v->data = realloc(v->data, v->capacity * sizeof(type));     \
     }                                                               \
     v->data[v->size++] = element;                                   \
 }                                                                   \
-void Vec_##type##_free(Vec_##type* v) {                             \
+void Vec_lt_##type##_gt_free(Vec_lt_##type##_gt* v) {                             \
     free(v->data);                                                  \
     v->data = NULL;                                                 \
 }                                                                   \
-size_t Vec_##type##_len(Vec_##type* v) {                            \
+size_t Vec_lt_##type##_gt_len(Vec_lt_##type##_gt* v) {                            \
     return v->size;                                                 \
 }
 
@@ -60,8 +60,8 @@ for (int name = 0; name < condition; name++) { \
 }
 
 #define FOR_EACH_LOOP_WITH_VEC(type, temp, item, arr, expression) \
-for (int temp = 0; temp < Vec_##type##_len(&arr); temp++) { \
-    type* item = Vec_##type##_get(&arr, temp); \
+for (int temp = 0; temp < Vec_lt_##type##_gt_len(&arr); temp++) { \
+    type* item = Vec_lt_##type##_gt_get(&arr, temp); \
     expression; \
 }
 
@@ -76,27 +76,29 @@ for (int temp = 0; temp < Vec_##type##_len(&arr); temp++) { \
 
 #define VEC_LITERAL_VAR(name, T, LEN, ...) \
     T temp_##name##_arr[] = { __VA_ARGS__ }; \
-    Vec_##T name = Vec_##T##_new(temp_##name##_arr, LEN);
+    Vec_lt_##T##_gt name = Vec_lt_##T##_gt_new(temp_##name##_arr, LEN);
 
 #define VEC_LITERAL_VAR_EMPTY(name, T) \
-    Vec_##T name = Vec_##T##_new(NULL, 0);
+    Vec_lt_##T##_gt name = Vec_lt_##T##_gt_new(NULL, 0);
 
 #define VEC_EMPTY_RETURN_PTR(type, temp) \
-    Vec_##type* temp = malloc(sizeof(Vec_##type)); \
-    *temp = Vec_##type##_new(NULL, 0); \
+    Vec_lt_##type##_gt* temp = malloc(sizeof(Vec_lt_##type##_gt)); \
+    *temp = Vec_lt_##type##_gt_new(NULL, 0); \
     return temp; 
 
 #define WITH_VEC(TYPE, NAME, TEMP_NAME, VALUES, LEN, CALL_EXPR) \
     TYPE TEMP_NAME[] = VALUES; \
-    Vec_##TYPE NAME = Vec_##TYPE##_new(TEMP_NAME, LEN); \
+    Vec_lt_##TYPE##_gt NAME = Vec_lt_##TYPE##_gt_new(TEMP_NAME, LEN); \
     CALL_EXPR; \
-    Vec_##TYPE##_free(&NAME); 
+    Vec_lt_##TYPE##_gt_free(&NAME); 
 
 #define VEC_LITTERAL_PTR(name, T, LEN, ...) \
     T temp_##name##_arr[] = { __VA_ARGS__ }; \
-    Vec_##T* name = malloc(sizeof(Vec_##T)); \
-    *name = Vec_##T##_new(temp_##name##_arr, LEN);
+    Vec_lt_##T##_gt* name = malloc(sizeof(Vec_lt_##T##_gt)); \
+    *name = Vec_lt_##T##_gt_new(temp_##name##_arr, LEN);
 
+#define GET_TUPLE_VAR(type, name, ...) \
+    type name = (type){ __VA_ARGS__ }; 
 
 typedef struct _String {
     char* data;
@@ -165,31 +167,32 @@ bool string_equals(string a, string b) {
 typedef bool* bool_ptr;
 typedef char* char_ptr;
 typedef float* float_ptr;
+typedef unsigned int uint;
 
 DEFINE_VEC(int);
-DEFINE_TUPLE(tuple_char_char, char a; char b;);
-DEFINE_VEC(tuple_char_char);
-DEFINE_VEC(Vec_tuple_char_char);
+DEFINE_TUPLE(tuple_lp_char_char_rp, char a; char b;);
+DEFINE_VEC(tuple_lp_char_char_rp);
+DEFINE_VEC(Vec_lt_tuple_lp_char_char_rp_gt);
 DEFINE_VEC(char);
 DEFINE_VEC(bool);
 DEFINE_VEC(bool_ptr);
 
 int hmm() {}
-void use_array(Vec_char _) {}
-int fancy_function(int a, int b, int c, Vec_int d) {}
+void use_array(Vec_lt_char_gt _) {}
+int fancy_function(int a, int b, int c, Vec_lt_int_gt d) {}
 int something() {}
 void nothing() {}
-Vec_int Vec_int_select__(Vec_int *a, int (*fn)(int)) {}
+Vec_lt_int_gt Vec_lt_int_gt_select__(Vec_lt_int_gt *a, int (*fn)(int)) {}
 void main() {}
 
 //ArrayExpression
 // pub struct Vec<T> { Y: select<Y>(Fn<Y, T>: fn) where T = int, Y = char
-Vec_char Vec_int_select_char(Vec_int *v, char (*fn)(int)) {
+Vec_lt_char_gt Vec_lt_int_gt_select_char(Vec_lt_int_gt *v, char (*fn)(int)) {
     VEC_LITERAL_VAR_EMPTY(result, char);
-    for (int temp1 = 0; temp1 < Vec_int_len(v); temp1++) {
+    for (int temp1 = 0; temp1 < Vec_lt_int_gt_len(v); temp1++) {
         int temp2 = v->data[temp1];
         char item = fn(temp2);
-        Vec_char_push(&result, item);
+        Vec_lt_char_gt_push(&result, item);
     }
     return result;
 }
@@ -200,17 +203,17 @@ void select_test() {
     // i32[]: arr = [1, 2, 3, 4, 5];
     VEC_LITERAL_VAR(arr, int, 5, 1, 2, 3, 4, 5);
     // char[]: result = arr.select(x => x as char);
-    Vec_char result = Vec_int_select_char(&arr, lambda1);
+    Vec_lt_char_gt result = Vec_lt_int_gt_select_char(&arr, lambda1);
 
     // if another value needs this, it will need to clone it
     //some_ther_value = result.clone();
 
     // free(arr);
-    Vec_int_free(&arr);
-    Vec_char_free(&result);
+    Vec_lt_int_gt_free(&arr);
+    Vec_lt_char_gt_free(&result);
 }
 
-Vec_bool* get_bool_array() {
+Vec_lt_bool_gt* get_bool_array() {
     VEC_LITTERAL_PTR(temp1, bool, 3, false, true, false);
     return temp1;
 }
@@ -219,13 +222,13 @@ void arrays() {
     // i32[]: arr = [];
     VEC_LITERAL_VAR_EMPTY(arr, int);
     // arr.push(99);
-    Vec_int_push(&arr, 99);
+    Vec_lt_int_gt_push(&arr, 99);
     
     // (u8, u8)[][]: b = [[(0, 0)], [], [(1, 1)]];
-    VEC_LITERAL_VAR(temp1, tuple_char_char, 1, (0, 0));
-    VEC_LITERAL_VAR_EMPTY(temp2, tuple_char_char);
-    VEC_LITERAL_VAR(temp3, tuple_char_char, 1, (1, 1));
-    VEC_LITERAL_VAR(b, Vec_tuple_char_char, 3, temp1, temp2, temp3);
+    VEC_LITERAL_VAR(temp1, tuple_lp_char_char_rp, 1, (0, 0));
+    VEC_LITERAL_VAR_EMPTY(temp2, tuple_lp_char_char_rp);
+    VEC_LITERAL_VAR(temp3, tuple_lp_char_char_rp, 1, (1, 1));
+    VEC_LITERAL_VAR(b, Vec_lt_tuple_lp_char_char_rp_gt, 3, temp1, temp2, temp3);
     
     // use_array([0, 1, 2])
     WITH_VEC(char, temp4, temp5, BRACE(0, 1, 2), 3, use_array(temp4));
@@ -235,19 +238,19 @@ void arrays() {
     // bool*[]: bp_array: [&val]; 
     VEC_LITERAL_VAR(bp_array, bool_ptr, 1, &val);
     //bool*[]*: bp_array_ptr = &bp_array;
-    Vec_bool_ptr* bp_array_ptr = &bp_array;
+    Vec_lt_bool_ptr_gt* bp_array_ptr = &bp_array;
     // free(bp_array_ptr);
-    Vec_bool_ptr_free(bp_array_ptr);
+    Vec_lt_bool_ptr_gt_free(bp_array_ptr);
 
     // bool[]: bools = get_bool_array();
-    Vec_bool* bools = get_bool_array();
+    Vec_lt_bool_gt* bools = get_bool_array();
     FOR_EACH_LOOP_WITH_VEC(bool, temp6, i, *bools, printf("%d", b));
 
-    Vec_bool_free(bools);
-    Vec_int_free(&arr);
+    Vec_lt_bool_gt_free(bools);
+    Vec_lt_int_gt_free(&arr);
 
-    Vec_Vec_tuple_char_char_free(&b);
-    Vec_bool_ptr_free(&bp_array);
+    Vec_lt_Vec_lt_tuple_lp_char_char_rp_gt_gt_free(&b);
+    Vec_lt_bool_ptr_gt_free(&bp_array);
 }
 //AsCast
 void as_cast() {
@@ -305,10 +308,10 @@ void discard() {
     // i32: arr = [1, 2, 3];
     // for _ in arr { nothing(); }
     WITH_VEC(int, arr, temp1, BRACE(1, 2, 3), 3, {
-        for (int temp2 = 0; temp2 < Vec_int_len(&arr); temp2++) {
+        for (int temp2 = 0; temp2 < Vec_lt_int_gt_len(&arr); temp2++) {
             nothing();
         }
-        Vec_int_select__(&arr, lambda2);
+        Vec_lt_int_gt_select__(&arr, lambda2);
     });
 }
 //EnumDeclaration
@@ -327,19 +330,19 @@ void for_loop() {
     VEC_LITERAL_VAR_EMPTY(n, int);
     REGULAR_FOR_LOOP(i, 10, {
         FOR_LOOP(int j = 0, j < 10, j = j + 2, {
-            Vec_int_push(&n, i * j);
+            Vec_lt_int_gt_push(&n, i * j);
         })
     });
-    FOR_LOOP_WITH_INDEX(index, int i = Vec_int_len(&n) / 4, i < Vec_int_len(&n) * 0.75, i = i + index, {
-        printf("%d", Vec_int_get(&n, i));        
+    FOR_LOOP_WITH_INDEX(index, int i = Vec_lt_int_gt_len(&n) / 4, i < Vec_lt_int_gt_len(&n) * 0.75, i = i + index, {
+        printf("%d", Vec_lt_int_gt_get(&n, i));        
     })
 }
 //ForEach
-Vec_tuple_char_char* z() { 
-    VEC_EMPTY_RETURN_PTR(tuple_char_char, temp1); 
+Vec_lt_tuple_lp_char_char_rp_gt* z() { 
+    VEC_EMPTY_RETURN_PTR(tuple_lp_char_char_rp, temp1); 
 }
-Vec_int int_as_range(int* this) {
-    return Vec_int_new(NULL, *this);
+Vec_lt_int_gt int_as_range(int* this) {
+    return Vec_lt_int_gt_new(NULL, *this);
 }
 void for_each_loop() {
     WITH_VEC(bool, temp1, temp2, BRACE(true, false, false, true), 4, {
@@ -348,22 +351,68 @@ void for_each_loop() {
         });
     });
     
-    Vec_tuple_char_char* temp4 = z();
-    FOR_EACH_LOOP_WITH_VEC(tuple_char_char, temp5, as_tuple_x_y, *temp4, {
+    Vec_lt_tuple_lp_char_char_rp_gt* temp4 = z();
+    FOR_EACH_LOOP_WITH_VEC(tuple_lp_char_char_rp, temp5, as_tuple_x_y, *temp4, {
         printf("%d", as_tuple_x_y->a);
         printf("%d", as_tuple_x_y->b);
     });
-    Vec_tuple_char_char_free(temp4);
+    Vec_lt_tuple_lp_char_char_rp_gt_free(temp4);
 
     int collection = 100;
-    Vec_int temp6 = int_as_range(&collection);
+    Vec_lt_int_gt temp6 = int_as_range(&collection);
     FOR_EACH_LOOP_WITH_VEC(int, i, item, temp6, {
         printf("%d", i);
     });
-    Vec_int_free(&temp6);
+    Vec_lt_int_gt_free(&temp6);
 }
 //FunctionCall
+#define DEFINE_GENERICS_3_2(T, G, F) \
+T generics_lt_##T##_##G##_##F##_gt(G g, F f) { \
+    T result = (T)g; \
+    result = result + (T)f; \
+    return result; \
+}
+DEFINE_GENERICS_3_2(int, float, uint)
+int add(int a, int b) {
+    return a + b;
+}
+void function_call() {
+    int result = add(1, 2);
+    printf("%d", result);
+    Vec_lt_tuple_lp_char_char_rp_gt* aaa = z();
+    int bbb = generics_lt_int_float_uint_gt(3.14, 42);
+    Vec_lt_tuple_lp_char_char_rp_gt_free(aaa);
+}
 //FunctionDeclaration
+#define DEFINE_PARSE_1_1(T) T parse_lt_##T##_rt(string s) {}
+DEFINE_PARSE_1_1(int)
+
+#define DEFINE_LOT_3_1(T, G, Q) \
+DEFINE_TUPLE(tuple_lp_##T##_##Q##_rp, T a; Q b;); \
+DEFINE_TUPLE(tuple_lp_##T##_##G##_rp, T a; G b;); \
+typedef tuple_lp_##T##_##G##_rp* tuple_lp_##T##_##G##_rp_ptr; \
+DEFINE_VEC(tuple_lp_##T##_##G##_rp); \
+typedef Vec_lt_tuple_lp_##T##_##G##_rp_gt* Vec_lt_tuple_lp_##T##_##G##_rp_gt_ptr; \
+Vec_lt_tuple_lp_##T##_##G##_rp_gt_ptr lot_lt_##T##_##G##_##Q##_gt(tuple_lp_##T##_##Q##_rp g) { \
+    Vec_lt_tuple_lp_##T##_##G##_rp_gt_ptr result = (Vec_lt_tuple_lp_##T##_##G##_rp_gt_ptr)malloc(sizeof(T) + sizeof(G)); \
+    tuple_lp_##T##_##G##_rp_ptr temp1 = Vec_lt_tuple_lp_##T##_##G##_rp_gt_get(result, 0); \
+    temp1->a = g.a; \
+    return result; \
+};
+
+DEFINE_LOT_3_1(int, float, string);
+
+void function_declaration() {
+    STRING_LIFETIME(temp, string_new("123"), 
+        int a = parse_lt_int_rt(temp);
+    );
+
+    STRING_LIFETIME(temp2, string_new("string"), 
+        GET_TUPLE_VAR(tuple_lp_int_string_rp, temp3, 25, temp2);
+        Vec_lt_tuple_lp_int_float_rp_gt_ptr b = lot_lt_int_float_string_gt(temp3);
+        Vec_lt_tuple_lp_int_float_rp_gt_free(b);
+    );
+}
 //Identifier
 //Indexer
 //If
